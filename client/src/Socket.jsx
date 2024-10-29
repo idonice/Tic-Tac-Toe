2383974f7101cf2e24a1b47a4f386b28519dede0
idonice <idonice3@gmail.com>
2024-10-29 18:04:45 +0200

    fixing

--- client/src/Socket.jsx
+++ client/src/Socket.jsx
@@ -1,5 +1,4 @@
 import io from 'socket.io-client';
-<<<<<<< HEAD
 const SOCKET_URL =
     process.env.NODE_ENV === 'production'
         ? 'https://vercel.com/idonices-projects/tic-tac-toe-server/'
@@ -7,6 +6,3 @@ const SOCKET_URL =
 
 
 export default io(SOCKET_URL);
-=======
-export default io('https://tic-tac-toe-backend-8p9l.onrender.com');
->>>>>>> 7a5ea2cb35175e749c7af4c692022de6ca6da39c
