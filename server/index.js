480335a9be7b113f24fc2ca627e89eafcefc89f1
idonice <idonice3@gmail.com>
2024-11-20 10:49:25 +0200

    Prepared app for deployment

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
 const corsOptions = {
     origin: ['https://tic-tac-g1plz2v4e-idonices-projects.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST'],
     credentials: true
 };
+
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*", methods: "*" } });
 const PORT = process.env.PORT || 5000;
 const rooms = {};
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-});
 
 app.use(cors(corsOptions));
 io.on('connection', socket => {
